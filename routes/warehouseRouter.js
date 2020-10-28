7cc0893f953494caba2459737a561f36918e0c46
Prakash <prakashsingha@gmail.com>
2020-10-28 18:43:28 +0800

    delete warehouse

--- routes/warehouseRouter.js
+++ routes/warehouseRouter.js
@@ -3,29 +3,49 @@ const express = require('express');
 function routes(Warehouse) {
   const warehouseRouter = express.Router();
 
-  warehouseRouter.route('/warehouses/:id').get((req, res) => {
+  warehouseRouter.use('/warehouses/:id', (req, res, next) => {
     Warehouse.findById(req.params.id, (err, warehouse) => {
       if (err) {
-        return res.send(err);
+        return res.status(500).send(err);
       }
-      return res.json(warehouse);
+      if (warehouse) {
+        req.warehouse = warehouse;
+        return next();
+      }
+      return res.sendStatus(404);
     });
   });
+
+  warehouseRouter
+    .route('/warehouses/:id')
+    .get((req, res) => res.json(req.warehouse))
+    .delete((req, res) => {
+      req.warehouse.remove((err) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+        return res.sendStatus(204);
+      });
+    });
 
   warehouseRouter
     .route('/warehouses')
     .get((req, res) => {
       Warehouse.find((err, warehouses) => {
         if (err) {
-          return res.send(err);
+          return res.status(500).send(err);
         }
-        return res.json(warehouses);
+        return res.status(200).json(warehouses);
       });
     })
     .post((req, res) => {
-      const wh = new Warehouse(req.body);
-      wh.save();
-      res.status(201).json(wh);
+      const warehouse = new Warehouse(req.body);
+      warehouse.save((err) => {
+        if (err) {
+          return res.status(500).send(err);
+        }
+        return res.status(201).json(warehouse);
+      });
     });
 
   return warehouseRouter;
