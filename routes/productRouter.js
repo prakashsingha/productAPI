a79eb8a97ad927c716cc85f6ba13a439520fee74
Prakash <prakashsingha@gmail.com>
2020-10-29 12:09:31 +0800

    Set verifyToken middleware for products and warehouses route

--- routes/productRouter.js
+++ routes/productRouter.js
@@ -1,19 +1,23 @@
 const express = require('express');
 
 const productController = require('../controllers/productController');
+const auth = require('../middlewares/auth')();
 
 function routes(Product) {
   const productRouter = express.Router();
   const controller = productController(Product);
 
-  productRouter.use('/products/:id', controller.getProductMiddleware);
-
+  productRouter.use('/products/:id', [
+    controller.getProductMiddleware,
+    auth.verifyToken
+  ]);
   productRouter
     .route('/products/:id')
     .get((req, res) => res.json(req.product))
     .patch(controller.updateProduct)
     .delete(controller.deleteProduct);
 
+  productRouter.use('/products', auth.verifyToken);
   productRouter
     .route('/products')
     .get(controller.searchProduct)
--- routes/warehouseRouter.js
+++ routes/warehouseRouter.js
@@ -1,18 +1,22 @@
 const express = require('express');
 
 const warehouseController = require('../controllers/warehouseController');
+const auth = require('../middlewares/auth')();
 
 function routes(Warehouse) {
   const warehouseRouter = express.Router();
   const controller = warehouseController(Warehouse);
 
-  warehouseRouter.use('/warehouses/:id', controller.getWarehouseMiddleware);
-
+  warehouseRouter.use('/warehouses/:id', [
+    controller.getWarehouseMiddleware,
+    auth.verifyToken
+  ]);
   warehouseRouter
     .route('/warehouses/:id')
     .get((req, res) => res.json(req.warehouse))
     .delete(controller.deleteWarehouse);
 
+  warehouseRouter.use('/warehouses', auth.verifyToken);
   warehouseRouter
     .route('/warehouses')
     .get(controller.searchWarehouse)
